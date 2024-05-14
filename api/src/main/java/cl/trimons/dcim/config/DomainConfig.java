baf96d0386193ae800f98f40886b3e8fe5ec0409
Víctor Morales <victormoraleslagos@gmail.com>
2024-05-14 18:03:07 -0400

    init database

--- api/src/main/java/cl/trimons/dcim/config/DomainConfig.java
+++ api/src/main/java/cl/trimons/dcim/config/DomainConfig.java
@@ -1,12 +1,14 @@
 package cl.trimons.dcim.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
+@ComponentScan("cl.trimons.dcim.model")
 @EntityScan("cl.trimons.dcim.domain")
 @EnableJpaRepositories("cl.trimons.dcim.repos")
 @EnableTransactionManagement
